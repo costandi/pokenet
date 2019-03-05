c02e6d48977f264224c370cd0e5bb89f7f5cd3eb
CelestBoy <46927514+CelestBoy@users.noreply.github.com>
2019-03-05 10:21:47 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -9,15 +9,26 @@
 
 
 <body>
-	<h1>Bienvenue sur pokenet !</h1>
-	<p>Pokenet est un site où vous pouvez constituer une équipe de pokemon et combattre d'autres joueurs dans le monde entier (votre salon) !</p>
+	<div class="round-button">
+		<a href="index.php">
+			<img src="pokeball.png" />
+		</a>
+	</div>
 
+	<h1 id="titre" >Bienvenue sur pokenet !</h1>
+
+	<div id="content">
+		<p id="resume" >Pokenet est un site où vous pouvez constituer une équipe de pokemon et combattre d'autres joueurs dans le monde entier (votre salon) !</p>
+	</div>
 	<form>
 		<button class="favorite styled" type="submit" formaction="inscrit.php" formmethod="POST" >Inscrivez vous !</button>
-		<button class="favorite styled" type="submit" formaction="connect.php" formmethod="POST" >Connectez vous !</button>
+		<button class="favorite styled" type="submit" formaction="connect.php" formmethod="POST" autofocus>Connectez vous !</button>
 	</form>
 
 	<img id="gifPika" src="pika.gif">
+
+
+
 
 </body>
 
@@ -32,7 +43,25 @@
 	padding: 0;
 	margin: 0;
 }
+
 */
+
+
+#titre {
+	position: fixed;
+	top: 0px;
+	left: 120px;
+	width: 300px;
+	/*border: 3px solid #73AD21;*/
+}
+
+#content {
+	border-radius: 5px;
+	border: 3px solid red;
+	margin: 10px;
+	margin-right: 900px;
+	padding-left: 5px;
+}
 
 .styled {
 	border: 0;
@@ -50,6 +79,7 @@
 		rgba(0, 0, 0, 0));
 	box-shadow: inset 2px 2px 3px rgba(255, 255, 255, .6),
 	inset -2px -2px 3px rgba(0, 0, 0, .6);
+
 }
 
 .styled:hover {
@@ -59,6 +89,26 @@
 .styled:active {
 	box-shadow: inset -2px -2px 3px rgba(255, 255, 255, .6),
 	inset 2px 2px 3px rgba(0, 0, 0, .6);
+}
+
+.round-button {
+	width: 80px;
+	height: 0;
+	padding-bottom: 80px;
+	border-radius: 50%;
+	border: 2px solid #f5f5f5;
+	overflow: hidden;
+	background: #464646;
+	box-shadow: 0 0 3px gray;
+}
+.round-button:hover {
+	background: red;
+}
+.round-button img {
+	display: block;
+	width: 80px;
+	padding: 0px;
+	height: auto;
 }
 
 #gifPika {
