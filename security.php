943d17e510d9ab549436622717259aa5134a64fc
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-03-21 15:29:52 +0100

    security.php mit et traitement.php upload
    Security.php contient une fonction pour echaper les caractère speciaux, une pour crypter et une pour encrypter les mot de passe.

---
+++ security.php
@@ -0,0 +1,42 @@
+<?php
+//Fonction qui verifie que le mot de passe nepose pas de probleme en échapant les caractères relous
+//Fonction personelle
+function mdpValid($MdP1)
+{
+    
+    $MdP2 = filter_var($MdP1, FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    if($MdP2 == false || $MdP1 != $MdP2)
+        return 1;
+    
+    else
+        return 0;
+}
+
+//Fonction qui encrypte des données avec un message a crypter & une clef.
+//Source : https://openclassrooms.com/fr/courses/2644516-securisez-les-mots-de-passe-des-utilisateurs-avec-php
+function encrypt($pure_string, $encryption_key) {
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+    $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+
+    return $encrypted_string;
+
+}
+
+//Fonction qui decrypte des données avec un message a décrypter & une clef.
+//Source : https://openclassrooms.com/fr/courses/2644516-securisez-les-mots-de-passe-des-utilisateurs-avec-php
+function decrypt($encrypted_string, $encryption_key) {
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+    $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, $encrypted_string, MCRYPT_MODE_ECB, $iv);
+
+    return $decrypted_string;
+}
+?>
--- traitement.php
+++ traitement.php
@@ -1,24 +1,31 @@
 <?php
+
+include './security.php';
+
 if (isset($_POST["connexion"]) && isset($_POST["nom"]))
 {
-	echo "connecté";
-	echo "<p>Bienvenue ".$_POST["nom"]." !</p>";
-	/*lien vers le jeu en php*/
+    echo "connecté";
+    echo "<p>Bienvenue ".$_POST["nom"]." !</p>";
+    /*lien vers le jeu en php*/
 }
 else if (isset($_POST["connexion"]) && !isset($_POST["nom"])) 
 {
-	echo "erreur de connexion";
-	/*lien vers l'accueil en prevenant pourquoi*/
+    echo "erreur de connexion";
+    /*lien vers l'accueil en prevenant pourquoi*/
 }
 
 if (isset($_POST["inscription"]))
 {
-	echo "inscrit sous le nom de ".$_POST["nom"]." ! ton mdp est ".$_POST["mdp"];
+    $check = mdpValid($_POST["mdp"]);
+    if ($check == 0)
+        echo "inscrit sous le nom de ".$_POST["nom"]." ! ton mdp est ".$_POST["mdp"];
+    else
+        echo "mot de passe invalide";
 }
 
 else if (isset($_POST["inscription"]) && !isset($_POST["connexion"]))
 {
-	echo "erreur d'inscription";
+    echo "erreur d'inscription";
 }
 
 
@@ -26,42 +33,42 @@ else if (isset($_POST["inscription"]) && !isset($_POST["connexion"]))
 
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
 	<title></title>
-</head>
-<body>
+    </head>
+    <body>
 	<div class="round-button" name="boutonPokeball">
-		<a href="index.php">
-			<img src="pokeball.png">
-		</a>
+	    <a href="index.php">
+		<img src="pokeball.png">
+	    </a>
 	</div>
-</body>
+    </body>
 </html>
 
 
 
 <style type="text/css">
 
-.round-button {
-	position: relative;
-	margin:5px;
-	width: 80px;
-	height: 0;
-	padding-bottom: 80px;
-	border-radius: 50%;
-	border: 2px solid #f5f5f5;
-	overflow: hidden;
-	background: #464646;
-	box-shadow: 0 0 3px gray;
-}
-.round-button:hover {
-	background: red;
-}
-.round-button img {
-	display: block;
-	width: 77px;
-	padding: 0;
-	height: auto;
-}
+ .round-button {
+     position: relative;
+     margin:5px;
+     width: 80px;
+     height: 0;
+     padding-bottom: 80px;
+     border-radius: 50%;
+     border: 2px solid #f5f5f5;
+     overflow: hidden;
+     background: #464646;
+     box-shadow: 0 0 3px gray;
+ }
+ .round-button:hover {
+     background: red;
+ }
+ .round-button img {
+     display: block;
+     width: 77px;
+     padding: 0;
+     height: auto;
+ }
 
 </style>
