ab3033872f77dfbe5e8a260b7660395b1e6f6480
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-03-24 09:44:44 +0100

    Nettoyage

--- Attaques.sql
+++
@@ -1,8 +0,0 @@
-insert into Attaques (Nom_ATK, Type, Degats)
-	values("Griffe", 1, 6);
-
-insert into Attaques (Nom_ATK, Type, Degats)
-	values("Charge", 1, 6);
-
-insert into Attaques (Nom_ATK, Type, Degats)
-	values("Lance-flamme", 2, 10);
