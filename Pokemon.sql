a0cabe67998be94fc5051d257f05193552a28282
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-03-24 09:47:16 +0100

    Nettoyage

--- Pokemon.sql
+++
@@ -1,9 +0,0 @@
-insert into Pokemon (ID_Pkd, Atk1, Atk2, Atk3, Atk4, Niveau, PV, Etat, KO, vitesse, sauvage)
-	values (1, 2, NULL, NULL, NULL, 5, 100, 6, 0, 2, 0);
-
-insert into Pokemon (ID_Pkd, Atk1, Atk2, Atk3, Atk4, Niveau, PV, Etat, KO, vitesse, sauvage)
-	values (4, 3, NULL, NULL, NULL, 5, 100, 6, 0, 2, 0);
-
-insert into Pokemon (ID_Pkd, Atk1, Atk2, Atk3, Atk4, Niveau, PV, Etat, KO, vitesse, sauvage)
-	values (7, 1, NULL, NULL, NULL, 5, 100, 6, 0, 2, 0);
-
