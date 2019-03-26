8cd3f5a3e4f17ef1894b77f724bd1ea4e901cfce
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-03-26 09:32:17 +0100

    Add files via upload

--- bdd.php
+++ bdd.php
@@ -9,9 +9,13 @@
        function CreUser($BDD, $username, $MdP);
 
 //Fermeture de la BDD
-//fonction personelle pour les test.
+//fonction personelle pour les test. A modifier
 
        function fermerBDD();
+
+//Verifier si un mot de passe crypter correspond au mot de passe de la base de donnée. Prend en paramettre une base de donnée, un nom d'utilisateur et le mot de passe a comparer a la base de donnée
+//Fonction personelle.
+       function checkUserBDD($BDD, $UNcheck, $MDP2check);
 */
 //__________________________________________________________________________________
 
@@ -28,6 +32,7 @@ function GenerBDD(){
 function CreUser($BDD, $username, $MdP){
     
     $prepUser = mysqli_prepare($BDD, "INSERT INTO User(UserName,User_MDP) VALUES(?,?)");
+
     mysqli_stmt_bind_param($prepUser, 'ss', $username, $MdP);
     mysqli_execute($prepUser);
 }
@@ -36,5 +41,22 @@ function fermerBDD($BDD){
     mysqli_close($BDD);
 }
 
+function checkUserBDD($BDD, $UNcheck, $MDP2check){
+    $stmt = mysqli_prepare($BDD, "SELECT User_MDP FROM User WHERE UserName = ?");
+    mysqli_stmt_bind_param($stmt, 's', $UNcheck);
+    mysqli_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $MDPvalide);
+    
+    while(mysqli_stmt_fetch($stmt));
+
+    echo "Vrai mdp= ".$MDPvalide."<br>MDP 2 look: ".$MDP2check."<br>";
+
+    
+    if ($MDPvalide == $MDP2check)
+        return true;
+    else
+        return false;
+    
+}
 
 ?>
