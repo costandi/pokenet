d4c2d5fe6b82b5e71c99e69a20442c9f013c9046
CelestBoy <46927514+CelestBoy@users.noreply.github.com>
2019-04-27 18:01:59 +0200

    Add files via upload

---
+++ BDD/PoType.sql
@@ -0,0 +1,45 @@
+insert into PoType
+	values (1, 4);
+
+insert into PoType
+	values (1, 8);
+
+insert into PoType
+	values (2, 4);
+
+insert into PoType
+	values (2, 8);
+
+insert into PoType
+	values (3, 4);
+
+insert into PoType
+	values (3, 8);
+
+/*---------------------------------*/
+
+insert into PoType
+	values (4, 2);
+
+insert into PoType
+	values (5, 2);
+
+insert into PoType
+	values (6, 2);
+
+insert into PoType
+	values (6, 10);
+
+/*---------------------------------*/
+
+insert into PoType
+	values (7, 2);
+
+insert into PoType
+	values (8, 2);
+
+insert into PoType
+	values (9, 2);
+
+/*---------------------------------*/
+
