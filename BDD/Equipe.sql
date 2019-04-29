3c72eb760df444df29a0e774287578941ed85970
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-04-29 09:48:56 +0200

    On avais oublier de modifier Equipe.sql lol

--- BDD/Equipe.sql
+++ BDD/Equipe.sql
@@ -1,15 +1,15 @@
 insert into Equipe
-	values (1, 1);
+	values (1, 1, 1);
 
 insert into Equipe
-	values (1, 2);
+	values (1, 2, 2);
 
 insert into Equipe
-	values (1, 3);
+	values (1, 3, 3);
 
 insert into Equipe
-	values (2, 4);
+	values (2, 4, 1);
 
 insert into Equipe
-	values (2, 5);
+	values (2, 5, 2);
 
