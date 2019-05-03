d2a14004bace7d5a5d6fb38961feb9826b02bf9d
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-05-03 15:28:22 +0200

    hey, listen!

--- ajaxShop.php
+++ ajaxShop.php
@@ -4,21 +4,32 @@ session_start();
 include './bdd.php';
 
 $BDD = GenerBDD();
+$rep = array();
 
 if($_POST["action"] == "1"){
     $tmp = buyPotion($BDD, $_SESSION['ID']);
+    $mnn = getMoney($BDD, $_SESSION["ID"]);
     $_POST = null;
     if ($tmp == true)
-	echo "Potion achetée!";
-    else echo "Tu n'a pas asser d'argent!";
+	$rep[0] = "Potion achetée! Il te reste ".$mnn." pokedollards.";
+    else $rep[0] = "Tu n'a pas asser d'argent!";
 }
 
 else if($_POST["action"] == "2"){
     $tmp = buyPokeball($BDD, $_SESSION['ID']);
+    $mnn = getMoney($BDD, $_SESSION["ID"]);
     $_POST = null;
     if ($tmp == true)
-	echo "Pokeball achetée!";
-    else echo "Tu n'a pas asser d'argent!";
+	$rep[0] = "Pokeball achetée! Il te reste ".$mnn." pokedollards.";
+    else $rep[0] = "Tu n'a pas asser d'argent!";
 }
 
+
+$rep[1] = getPokeball($BDD, $_SESSION['ID']);
+$rep[2] = getPotion($BDD, $_SESSION['ID']);
+$rep[3] = $mnn;
+
+fermerBDD($BDD);
+
+echo json_encode($rep);
 ?>
