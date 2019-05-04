6e3a4fb33f383fb33afbfde674dedc94179b3187
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-05-04 21:01:28 +0200

    I'm dumb.

--- jeu.php
+++ jeu.php
@@ -2,15 +2,6 @@
 session_start();
 
 include './bdd.php';
-$BDD = GenerBDD();
-
-$un = getUsername($BDD, $_SESSION['ID']);
-$pb = getPokeball($BDD, $_SESSION['ID']);
-$pt = getPotion($BDD, $_SESSION['ID']);
-$mn = getMoney($BDD, $_SESSION['ID']);
-
-fermerBDD($BDD);
-
 include './Template/header.php';?>
 Fenetre
 
