efcfb15db9e21a57a2a254237886a04cc1835cd2
cynthia-DL <46927498+cynthia-DL@users.noreply.github.com>
2019-05-09 19:05:38 +0200

    ajaxShop mis a jour

--- ajaxShop.php
+++ ajaxShop.php
@@ -13,8 +13,10 @@ if($_POST["action"] == "1"){
         $tmp = buyPotion($BDD, $_SESSION['ID']);
     
     $mnn = getMoney($BDD, $_SESSION["ID"]);
-
+    $i = $i -1;
+    
     if ($tmp == true || ($tmp == false && $i > 0)){
+        $i = $i+1;
         $rep[0] = "Tu a acheter ".$i." potion(s)! Il te reste ".$mnn." pokedollards.";
     }
     else $rep[0] = "Tu n'a pas asser d'argent!";
@@ -25,9 +27,12 @@ else if($_POST["action"] == "2"){
       $i < $_POST['qtte'] && $tmp != false;
       $i = $i + 1)
      $tmp = buyPokeball($BDD, $_SESSION['ID']);
+ 
     $mnn = getMoney($BDD, $_SESSION["ID"]);
-
+    $i = $i -1;
+    
     if ($tmp == true || ($tmp == false && $i > 0)){
+        $i = $i+1;
         $rep[0] = "Tu a acheter ".$i." pokeball(s)! Il te reste ".$mnn." pokedollards.";
     }
     else $rep[0] = "Tu n'a pas asser d'argent!";
