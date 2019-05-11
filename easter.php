dc150af9729e68b59472f19f4101f1e70cc57955
CelestBoy <46927514+CelestBoy@users.noreply.github.com>
2019-05-11 18:45:41 +0200

    Add files via upload

---
+++ easter.php
@@ -0,0 +1,37 @@
+<div class="round-button" name="boutonPokeball">
+		<a href="index.php">
+			<img src="decors/pokeball.png">
+		</a>
+	</div>
+
+
+<style type="text/css">
+	* {
+		background: url("decors/dance.gif");
+		margin : 0;
+		padding: 0;
+	}
+
+
+.round-button {
+    position: fixed;
+    margin:5px;
+    width: 80px;
+    height: 0;
+    padding-bottom: 80px;
+    border-radius: 50%;
+    border: 2px solid #f5f5f5;
+    overflow: hidden;
+    background: #464646;
+    box-shadow: 0 0 3px gray;
+}
+.round-button:hover {
+    background: red;
+}
+.round-button img {
+    display: block;
+    width: 77px;
+    padding: 0;
+    height: auto;
+}
+</style>
