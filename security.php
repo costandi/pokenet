30cd33d5f4d665e03d5034b89dd0ed9e9b19ab98
CelestBoy <46927514+CelestBoy@users.noreply.github.com>
2019-10-15 17:52:24 +0000

    nouveau security.php

--- security.php
+++ security.php
@@ -3,48 +3,33 @@
 //Fonction personelle
 function mdpValid($MdP1)
 {
-    
-    $MdP2 = filter_var($MdP1, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    
-    if($MdP2 == false || $MdP1 != $MdP2)
-        return 1;
-    
-    else
-        return 0;
+	
+	$MdP2 = filter_var($MdP1, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	
+	if($MdP2 == false || $MdP1 != $MdP2)
+		return 1;
+	
+	else
+		return 0;
 }
 
-//Source : https://openclassrooms.com/fr/courses/2091901-protegez-vous-efficacement-contre-les-failles-web/2873202-protegez-les-donnees
-class Chiffrement {
-    
-    // Algorithme utilisé pour le cryptage des blocs
-    private static $cipher  = MCRYPT_RIJNDAEL_128;
-    
-    // Clé de cryptage         
-    private static $key = "faucibusturpisineumi";
-    
-    // Mode opératoire (traitement des blocs)
-    private static $mode    = 'cbc';
-    
-    public static function crypt($data){
-        $keyHash = md5(self::$key);
-        $key = substr($keyHash, 0, mcrypt_get_key_size(self::$cipher, self::$mode));
-        $iv  = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode));
-        
-        $data = mcrypt_encrypt(self::$cipher, $key, $data, self::$mode, $iv);
-        
-        return base64_encode($data);
-    }
-    
-    public static function decrypt($data){
-        $keyHash = md5(self::$key);
-        $key = substr($keyHash, 0,   mcrypt_get_key_size(self::$cipher, self::$mode) );
-        $iv  = substr($keyHash, 0, mcrypt_get_block_size(self::$cipher, self::$mode) );
-        
-        $data = base64_decode($data);
-        $data = mcrypt_decrypt(self::$cipher, $key, $data, self::$mode, $iv);
-        
-        return rtrim($data);
-    }
+
+function cryptage($data){
+	$key='faucibusturpisineumi';
+	$method='ripemd160';
+
+	$data = hash_hmac($method, $data, $key);
+
+	return base64_encode($data);
 }
+
+function decrypt($mdpcrypte, $mdpentre){
+	$key='faucibusturpisineumi';
+	$method='ripemd160';
+
+	$mdpcrypte = base64_decode($mdpcrypte);
+	return password_verify($mdpcrypte, $mdpentre);
+}
+
 
 ?>
